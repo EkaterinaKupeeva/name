1f41e91196a96d064f40746b4608784394cc0692
Ekaterina Kupeeva <katas0991@gmail.com>
2023-06-19 15:03:41 +0300

    My first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { Navlink, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+
+const Profile = () => {
+  return <h1>Страница профиля</h1>
+}
+
+function App() {
+  return (
+    <div className="container">
+      <div className="col-5">
+        <div
+          className="nav flex-column nav-pills">
+          <Navlink
+            className="nav-link"
+            href="profile"
+          >
+            Профиль
+          </Navlink>
+          <Navlink
+            className="nav-link"
+            href="massages"
+          >
+            Сообщения
+          </Navlink>
+          <Navlink
+            className="nav-link"
+            href="settings"
+          >
+            Настройки
+          </Navlink>
+          <Navlink
+            className="nav-link"
+            href="friends"
+          >
+            Мои друзья
+          </Navlink>
+        </div>
+      </div>
+      <div className="col-9">
+        <Routes>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
