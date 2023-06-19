34a52e67739fbd6c2c5ec79364c1f77ecdbcf539
Ekaterina Kupeeva <katas0991@yandex.ru>
2023-06-19 18:10:48 +0300

    add active btn

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
-import { Navlink, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-
 const Profile = () => {
-  return <h1>Страница профиля</h1>
-}
+  return <h1>Страница профиля</h1>;
+};
+const Massages = () => {
+  return <h1>Страница сообщений</h1>;
+};
+const Settings = () => {
+  return <h1>Страница настроек</h1>;
+};
+const Friends = () => {
+  return <h1>Страница с друзьями</h1>;
+};
 
 function App() {
   return (
-    <div className="container">
-      <div className="col-5">
-        <div
-          className="nav flex-column nav-pills">
-          <Navlink
-            className="nav-link"
-            href="profile"
-          >
-            Профиль
-          </Navlink>
-          <Navlink
-            className="nav-link"
-            href="massages"
-          >
-            Сообщения
-          </Navlink>
-          <Navlink
-            className="nav-link"
-            href="settings"
-          >
-            Настройки
-          </Navlink>
-          <Navlink
-            className="nav-link"
-            href="friends"
-          >
-            Мои друзья
-          </Navlink>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-3">
+          <div className="nav flex-column nav-pills">
+            <NavLink className="nav-link" to="profile">
+              Профиль
+            </NavLink>
+            <NavLink className="nav-link" to="massages">
+              Сообщения
+            </NavLink>
+            <NavLink className="nav-link" to="settings">
+              Настройки
+            </NavLink>
+            <NavLink className="nav-link" to="friends">
+              Мои друзья
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-9">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/massages" element={<Massages />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/friends" element={<Friends />} />
+          </Routes>
         </div>
       </div>
-      <div className="col-9">
-        <Routes>
-          <Browser></Browser>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
-      </div>
     </div>
-
   );
 }
 
