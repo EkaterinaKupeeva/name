dc99c5ff1d749f0f59fd5c8927560ef9e0e405e9
Ekaterina Kupeeva <katas0991@yandex.ru>
2023-06-21 10:41:54 +0300

    add img

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import backgrImg from ".././assets/img/ger.jpg"
 
 const TableRow = (props) => {
 return (
@@ -31,7 +32,10 @@ export const Friends = (props) => {
         {userRow}
         </tbody>
         </table>
-
+        <div className="row mt-5">
+          <div className="col-xl-6 p-5 text-white" style={{backgroundImage: `url(${backgrImg})`}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto ipsam recusandae dolor illo eligendi molestiae ipsum porro minima harum in quis quisquam quos eveniet nemo ipsa, unde, perferendis sunt tenetur?</div>
+          <div className="col-xl-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero dolor nam, doloremque fugiat consequuntur rem aspernatur voluptate id alias fuga, distinctio quisquam, error dolorum minus accusamus? Accusantium odio dolorum saepe?</div>
+        </div>
       </div>
     );
   };
